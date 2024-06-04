7dc172d5d81ded58c14da2fbf134c1c447c57c05
root <root@G3RM4N>
2024-06-04 11:24:41 +0100

    Fix semistandard compliance errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,29 +6,25 @@ const request = require('request');
 const movieId = process.argv[2];
 
 if (!movieId) {
-    process.exit(1);
+  process.exit(1);
 }
 
 const url = `https://swapi.dev/api/films/${movieId}`;
 
 request(url, (error, response, body) => {
-    if (error) {
-        callback(error);
-    } else {
-        const data = JSON.parse(body);
-        const characters = data.characters;
-        fetchNextCharacter(characters, 0);
-    }
+  if (error) throw error;
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  fetchNextCharacter(characters, 0);
 });
 
-
 const fetchNextCharacter = (characters, index) => {
-    if (index === characters.length) return;
+  if (index === characters.length) return;
 
-    request(characters[index], (err, res, body) => {
-        if (err) throw err;
-        console.log(JSON.parse(body).name);
-        index++;
-        fetchNextCharacter(characters, index);
-    });
+  request(characters[index], (err, res, body) => {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    index++;
+    fetchNextCharacter(characters, index);
+  });
 };
