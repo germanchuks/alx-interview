ab880e7e81a54a8913d20a854f94be737c28bd1b
root <root@G3RM4N>
2024-06-04 11:20:29 +0100

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+// Script to print all characters of a Star Wars movie
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+    process.exit(1);
+}
+
+const url = `https://swapi.dev/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+    if (error) {
+        callback(error);
+    } else {
+        const data = JSON.parse(body);
+        const characters = data.characters;
+        fetchNextCharacter(characters, 0);
+    }
+});
+
+
+const fetchNextCharacter = (characters, index) => {
+    if (index === characters.length) return;
+
+    request(characters[index], (err, res, body) => {
+        if (err) throw err;
+        console.log(JSON.parse(body).name);
+        index++;
+        fetchNextCharacter(characters, index);
+    });
+};
